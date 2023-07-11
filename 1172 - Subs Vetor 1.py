8326dba47b89965a29481d11918fdc4b9ab8b5e4
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2023-07-11 18:32:37 -0300

    Pastas e arquivos adicionados

--- 1172 - Subs Vetor 1.py
+++
@@ -1,6 +0,0 @@
-for i in range(10):
-    num = int(input())
-    if num > 0:
-        print(f'X[{i}] = {num}')
-    elif num <= 0:
-        print(f'X[{i}] = 1')
--- 1235 - De Dentro para Fora.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-for i in range(n):
-    s1 = s2 = ""
-    frase = input()
-    s1 += frase[:len(frase)//2]
-    s2 += frase[len(frase)//2:]
-    s1 = s1[::-1]
-    s2 = s2[::-1]
-    print(s1, s2, sep="")
