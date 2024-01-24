5c8f8bc9e3d8c838cf93e536fcb6dd8e0909c6e8
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2024-01-24 16:30:06 -0300

    Promoçao

---
+++ C++/1624.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+int knapsack(v32 &numeros, v32 &pesos, int maxPeso, int n)
+{
+  if (!maxPeso || !n)
+    return 0;
+
+  if (pesos[n-1] > maxPeso) {
+    return knapsack(numeros, pesos, maxPeso, n-1);
+  } else {
+    return max(knapsack(numeros, pesos, maxPeso, n-1), 
+      numeros[n-1] + knapsack(numeros, pesos, maxPeso-pesos[n-1], n-1));
+  }
+}
+
+void solve(int n)
+{
+  int maxPeso;
+  v32 numeros(n), pesos(n);
+  for (int i = 0; i < n; i++) {
+    cin >> numeros[i] >> pesos[i];
+  }
+  cin >> maxPeso;
+  cout << knapsack(numeros, pesos, maxPeso, n) << "\n";
+}
+
+int main()
+{
+  int n;
+  while (true) {
+    cin >> n;
+    if (!n) break;
+
+    solve(n);
+  }
+}
