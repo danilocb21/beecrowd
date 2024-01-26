c65f983fb685c6f9e7034f0fd82037599314333f
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2024-01-26 13:36:30 -0300

    Maças

--- C++/1517.cpp
+++ C++/1517.cpp
@@ -52,8 +52,8 @@ typedef vector<p32> vp32;
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((ll)(x).size())
 
-int dp[21][21][1001];
-int matriz[21][21][1001];
+int dp[21][21][2002];
+int matriz[21][21][2002];
 
 int mb(int n, int m, int x, int y, int c, int k)
 {
@@ -63,15 +63,14 @@ int mb(int n, int m, int x, int y, int c, int k)
     if (c == k+1)
         return 0;
 
-    int cont = 0;
-    if (matriz[x][y][c] == 1)
-        cont = 1;
+    int cont;
+    (matriz[x][y][c] == 1) ? cont=1 : cont=0;
     
-    int movesX[] = {-1,-1,-1,0,0,1,1,1};
-    int movesY[] = {-1,0,1,-1,1,-1,0,1};
+    int movesX[] = {0,-1,-1,-1,0,0,1,1,1};
+    int movesY[] = {0,-1,0,1,-1,1,-1,0,1};
     
     int maior = -1;
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 9; i++) {
         if (x+movesX[i] < 1 || x+movesX[i] > n)
             continue;
         if (y+movesY[i] < 1 || y+movesY[i] > m)
@@ -91,13 +90,13 @@ void solve(int n, int m, int k)
     memset(dp, -1, sizeof dp);
     memset(matriz, 0, sizeof matriz);
     
+    int a, b, c;
     for (int i = 0; i < k; i++) {
-        int a, b, c;
         cin >> a >> b >> c;
         matriz[a][b][c] = 1;
     }
     int x, y; cin >> x >> y;
-    cout << mb(n, m, x, y, 0, k) << "\n";
+    cout << mb(n, m, x, y, 0, c) << "\n";
 }
 
 int main()
