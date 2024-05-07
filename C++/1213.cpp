88a965416f3162dab1b9d14d8766288695011e53
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2024-05-06 22:35:06 -0300

    Ones

---
+++ C++/1213.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std; 
+typedef long long ll;
+typedef unsigned long long ull;
+using i64 = long long;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+const int N=1000005;
+#define INF 0x3fffffff
+const int mod=1e9+7;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define endl '\n'
+using PII = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#ifndef ONLINE_JUDGE
+#include "template.cpp"
+#else
+#define debug(...)
+#define debugArr(arr, n)
+#endif
+
+int n;
+
+void solve()
+{   
+    int ans = 1;
+    ull fact = 1;
+    while (fact % n) {
+        fact = (10*fact+1) % n;
+        ans++;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_cin();
+    while (cin >> n) solve();
+}
