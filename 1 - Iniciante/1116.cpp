d6078afc1639a7c3d23bf9cd2eeb68a103717847
Danilo <danilocbjp@hotmail.com>
2024-12-25 11:19:20 -0300

    Dividindo X por Y

---
+++ 1 - Iniciante/1116.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+const int mod=(1e9)+7;
+#define PI 3.14159265358979323846
+#define yes cout<<"YES"<<endl
+#define Yes cout<<"Yes"<<endl
+#define no cout<<"NO"<<endl
+#define No cout<<"No"<<endl
+#define endl '\n'
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    double a,b;
+    cin >> a >> b;
+    if (b == 0) {
+        cout << "divisao impossivel\n";
+        return;
+    }
+    cout << setprecision(1) << fixed << a/b << endl;
+}
+
+int main() {
+    fast_cin();
+    int t; cin >> t;
+    while(t--) solve();
+}
