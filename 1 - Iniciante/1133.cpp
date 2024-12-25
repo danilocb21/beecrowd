ba02f16312bed3e7c0b6414fa1842385d1381a9b
Danilo <danilocbjp@hotmail.com>
2024-12-25 11:41:58 -0300

    Resto da Divisão

---
+++ 1 - Iniciante/1133.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+const int mod=(1e9)+7;
+#define PI 3.14159265358979323846
+#define yes cout<<"YES"<<endl
+#define Yes cout<<"Yes"<<endl
+#define no cout<<"NO"<<endl
+#define No cout<<"No"<<endl
+#define endl '\n'
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    int a,b;
+    cin >> a >> b;
+    if (a > b) swap(a,b);
+    for (int i = a+1; i < b; i++)
+        if (i%5 == 2 || i%5==3) cout << i << endl;
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
