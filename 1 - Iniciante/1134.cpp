3d2ba247b4c2bf528e8fc4c026a23de547cbad68
Danilo <danilocbjp@hotmail.com>
2024-12-25 11:47:17 -0300

    Tipo de Combustível

---
+++ 1 - Iniciante/1134.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+const int mod=(1e9)+7;
+#define PI 3.14159265358979323846
+#define yes cout<<"YES"<<endl
+#define Yes cout<<"Yes"<<endl
+#define no cout<<"NO"<<endl
+#define No cout<<"No"<<endl
+#define endl '\n'
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    v32 v(4);
+    int k = 0;
+    while (k != 4) {
+        cin >> k;
+        if (k < 1 || k > 3) continue;
+        v[k]++;
+    }
+    cout << "MUITO OBRIGADO\n";
+    printf("Alcool: %d\n", v[1]);
+    printf("Gasolina: %d\n", v[2]);
+    printf("Diesel: %d\n", v[3]);
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
