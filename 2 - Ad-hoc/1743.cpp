77d85d820edfc8d5eaa6b422a226f8ba41a98f36
Danilo <danilocbjp@hotmail.com>
2024-12-26 00:19:22 -0300

    Máquina de Verificação Automatizada

---
+++ 2 - Ad-hoc/1743.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+const int mod=(1e9)+7;
+#define PI 3.14159265358979323846
+#define yes cout<<"YES"<<endl
+#define Yes cout<<"Yes"<<endl
+#define no cout<<"NO"<<endl
+#define No cout<<"No"<<endl
+#define endl '\n'
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    vector<bool> a(5), b(5);
+    for (int i = 0; i < 5; i++) {
+        bool x; cin >> x;
+        a[i] = x;
+    }
+    for (int i = 0; i < 5; i++) {
+        bool x; cin >> x;
+        b[i] = x;
+    }
+    for (int i = 0; i < 5; i++) {
+        if (!(a[i]^b[i])) {
+            cout << "N\n";
+            return;
+        }
+    }
+    cout << "Y\n";
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
