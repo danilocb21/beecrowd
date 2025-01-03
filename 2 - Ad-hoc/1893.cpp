527c4b58976a3e1bce8d59a6b48dc26ae87e555a
Danilo <danilocbjp@hotmail.com>
2025-01-03 12:47:34 -0300

    Fases da Lua

---
+++ 2 - Ad-hoc/1893.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+const int mod=(1e9)+7;
+#define PI 3.14159265358979323846
+#define yes cout<<"YES"<<endl
+#define Yes cout<<"Yes"<<endl
+#define no cout<<"NO"<<endl
+#define No cout<<"No"<<endl
+#define endl '\n'
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    if (m <= 2)
+        cout << "nova\n";
+    else if (m >= 97)
+        cout << "cheia\n";
+    else if (m > n)
+        cout << "crescente\n";
+    else
+        cout << "minguante\n";
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
