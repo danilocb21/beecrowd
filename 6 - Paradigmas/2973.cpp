3d3e695c955cee61777154737ce048f8806ae959
Danilo <danilocbjp@hotmail.com>
2025-01-23 18:15:33 -0300

    Maratona Brasileira de Comedores de Pipoca

---
+++ 6 - Paradigmas/2973.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+const int mod=(1e9)+7;
+#define PI 3.14159265358979323846
+#define yes cout<<"YES"<<endl
+#define Yes cout<<"Yes"<<endl
+#define no cout<<"NO"<<endl
+#define No cout<<"No"<<endl
+#define endl '\n'
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mp make_pair
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    int n, c, t;
+    cin >> n >> c >> t;
+
+    v32 v(n);
+    for (auto &x : v)
+        cin >> x;
+    
+    int l = 1, r = 1e9;
+
+    while (l < r) {
+        int m = l + (r-l)/2;
+        ll total = 1ll * m * t;
+        ll qtd = 1;
+        for (int i = 0; i < n; i++) {
+            if (total >= v[i])
+                total -= v[i];
+            else {
+                total = m * t;
+                qtd++;
+                i--;
+            }
+            if (qtd > c) break;
+        }
+
+        if (qtd > c)
+            l = m+1;
+        else
+            r = m;
+    }
+
+    cout << l << endl;
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
