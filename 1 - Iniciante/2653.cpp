cbace572bb3809a4e1514a3b20bcb7062a791092
Danilo <danilocbjp@hotmail.com>
2025-05-29 11:27:16 -0300

    Dijkstra

---
+++ 1 - Iniciante/2653.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+#define INF 0x3fffffff
+#define LLINF 0x3fffffffffffffff
+#define PI 3.14159265358979323846
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define pb push_back
+#define fast_cin()                    \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    string s;
+    set<string> ans;
+    while (cin>>s)
+        ans.insert(s);
+    cout << sz(ans) << endl;
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
