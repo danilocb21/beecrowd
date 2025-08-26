df741a1661a3d2c7de33dc389b5d28fb3426f30d
Danilo <danilocbjp@hotmail.com>
2025-08-25 23:51:36 -0300

    Tartarugas

--- 2 - Ad-hoc/1518.cpp
+++ 2 - Ad-hoc/1518.cpp
@@ -5,64 +5,38 @@ using namespace std;
 using namespace __gnu_pbds;
 typedef long long ll;
 typedef unsigned long long ull;
-using i64 = long long;
 typedef long double ld;
 typedef vector<ll> v64;
 typedef vector<int> v32;
-#define INF INT_MAX
-#define LLINF LLONG_MAX
-const int mod=(1e9)+7;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+#define INF 0x3fffffff
+#define LLINF 0x3fffffffffffffff
 #define PI 3.14159265358979323846
-#define yes cout<<"YES"<<endl
-#define Yes cout<<"Yes"<<endl
-#define no cout<<"NO"<<endl
-#define No cout<<"No"<<endl
 #define endl '\n'
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
 #define F first
 #define S second
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((ll)(x).size())
-#define mp make_pair
-#define mt make_tuple
 #define pb push_back
-#define eb emplace_back
-#define fast_cin()                    \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+#define fast_cin()               \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
-ll pw(ll a, ll b)
-{
-    if (b == 0)
-        return 1;
-
-    if (b % 2) {
-        ll t = pw(a, (b-1)/2);
-        return (1ll * a * t * t);
-    }
-    else {
-        ll t = pw(a, b/2);
-        return (1ll * t * t);
-    }
-}
-
-struct avx {
+struct Point {
     int x,y;
     char c;
-    bool operator<(const avx& a) const {
+    bool operator<(const Point& a) const {
         if (x != a.x) return x < a.x;
-        if (y != a.y) return x < a.y;
-        return c < a.c;
+        return y < a.y;
     }
 };
 
 int x,y;
-void solve()
-{
-    cin>>x>>y;
-    vector<avx> v(3);
+void solve() {
+    vector<Point> v(3);
     for (int i = 0; i < 3; i++) {
         int a,b;
         char c;
@@ -71,19 +45,32 @@ void solve()
     }
     sort(all(v));
     int ans = INF;
-    while (next_permutation(all(v))) {
+    do {
         int a = x, b = y;
+        int cur = 0;
         for (int i = 0; i < 3; i++) {
-            int dx = abs(a-v[i].x);
-            int dy = abs(b-v[i].y);
+            for (int add = 1; add < 1e5; add++) {
+                int nx = (v[i].c == 'D' ? v[i].x+cur+add : v[i].x);
+                int ny = (v[i].c == 'C' ? v[i].y+cur+add : v[i].y);
+
+                int dx = abs(nx-a);
+                int dy = abs(ny-b);
+                int t_need = min(dx,dy) + (abs(dx-dy)+1)/2;
+                if (t_need <= add) {
+                    cur += add;
+                    a = nx;
+                    b = ny;
+                    break;
+                }
+            }
             
         }
-    }
+        ans = min(ans,cur);
+    } while (next_permutation(all(v)));
     cout << ans << endl;
 }
 
-int main()
-{
+int main() {
     fast_cin();
     while (cin>>x>>y && (x || y)) solve();
 }
