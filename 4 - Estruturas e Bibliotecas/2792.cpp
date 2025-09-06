29a722e145a1e7779cd8ee59041871f200bcccd2
Danilo <danilocbjp@hotmail.com>
2025-09-05 21:25:50 -0300

    Golnaldinho

---
+++ 4 - Estruturas e Bibliotecas/2792.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+#define INF 0x3fffffff
+#define LLINF 0x3fffffffffffffff
+#define PI 3.14159265358979323846
+#define endl '\n'
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define pb push_back
+#define fast_cin()               \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+template<class T> using ordered_set = 
+tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+void solve() {
+    int n;
+    cin>>n;
+
+    v32 v(n);
+    iota(all(v), 1);
+    
+    ordered_set<int> s;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin>>x;
+        cout << x - s.order_of_key(x) << " \n"[i==n-1];
+        s.insert(x);
+    }
+}
+
+int main() {
+    fast_cin();
+    solve();
+}
